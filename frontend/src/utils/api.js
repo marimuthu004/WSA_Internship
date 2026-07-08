f5a1305075dbffc403f935c15ac07b4d9f8abf59
Marimuthu K <krishnamarimuthu6@gmail.com>
2026-07-08 19:58:46 +0530

    Update Axios baseURL to use environment variable

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,12 +1,11 @@
-//centeralized API setup
-
 import axios from 'axios';
 import qs from 'qs';
 
 const api = axios.create({
-    baseURL: '/api',
+    // Use the live Render URL in production, or fallback to localhost for local testing
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
     paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }),
-})
+});
 
 export default api;
